699ffbcb2eaf39aa89f2ec8de28358aba7a442b1
JP <jpjones22@gmail.com>
2020-10-29 19:27:26 -0700

    Added  a small list of commands

--- xtTerm.js
+++ xtTerm.js
@@ -1,8 +1,7 @@
 'use strict'
 var maxString = 'Version 3.30 (C)Copyright International Business Machines Corp 1981, 1987';
-console.log(maxString.length);
-var userInput = [];
-var userInputString = '';
+var userInput = '';
+var firstReturn = true;
 var monitorArray = [];//['Initial OS 4a50 Systems', 'Initializing Boot Parameters', "[ OK ] Synching mainframe to digital domains", "Connected at (1200) bps", "Running TELNET query", "Response Received - 10b"];
 var monitorLineTracker = 0;
 var maxLinesOnScreen = 23;
@@ -11,31 +10,24 @@ var maxLinesOnScreen = 23;
 //Add all the elements of the monitorArray
 function initMonitor() {
   for (var i = 0; i < maxLinesOnScreen; i++) {
-    monitorArray.push('<br />');
-    $("#monitor-display").append(`<p id="line-${i}">${monitorArray[monitorArray.length - 1]}`);
+    $("#monitor-display").append(`<p id="line-${i}"><br />`);
   }
 }
 function addTextToLineScreen() {
   $(`#line-${monitorLineTracker}`).html(userInput);
 }
 function addNextLineToScreen() {
-  userInputString = userInput.join('');
-  console.log('userToString: ' + userInputString);
-  evalUserCommand();
-  userInput = [];
+
+  evalUserCommand(userInput.toUpperCase());
   monitorLineTracker++;
 }
-function evalUserCommand() {
-  console.log("Processing Request");
-}
-
-
-
+/// Main Function to determine what to do with the input
 function userInputManagement(charCode, nonText = false) {
   if (nonText) {
     if (charCode === 8) { //BackSpace      
       if (userInput.length > 0) {
-        userInput.pop();
+        console.log('BSPACE yo!');
+        userInput = userInput.slice(0, -1);
       }
     }
     else if (charCode === 13) { //ENTER
@@ -43,26 +35,61 @@ function userInputManagement(charCode, nonText = false) {
     }
   }
   else {
-    userInput.push(String.fromCharCode(charCode));
+    userInput += String.fromCharCode(charCode);
+    console.log(userInput);
 
   }
   addTextToLineScreen();
-  console.log(userInput);
+
+}
+function evalUserCommand(cmdEval) {
+  console.log("Processing Request");
+  monitorLineTracker++;
+  if (cmdEval.charCodeAt(0) === 13) { cmdEval = cmdEval.slice(1); } // Weeds out the CR that gets put into the string
+
+  switch (cmdEval) {
+    case 'CLS':
+      console.log('Clearing the Screen');
+      clearTheScreen();
+      break;
+    case 'JP.EXE':
+      $(`#line-${monitorLineTracker}`).html('-= JP =- is the creator of this little app');
+      break;
+    case 'CHKDSK':
+      $(`#line-${monitorLineTracker}`).html('I\'m not going to check storage.  To much work');
+      break;
+    case 'PARKER':
+      $(`#line-${monitorLineTracker}`).html('Ahh Yes!  The gentleman with the Axolotls!');
+      break;
+    default:
+
+      console.log('Bad command or filename');
+      $(`#line-${monitorLineTracker}`).html('Bad command or filename');
+      break;
+  }
+  monitorArray.push(userInput);
+  userInput = '';
 }
 
+function clearTheScreen() {
+  monitorArray = [];
+  monitorLineTracker = -1;
+  for (var i = 0; i < maxLinesOnScreen; i++) {
+    $(`#line-${i}`).html(`<br />`);
+  }
 
+
+}
 //Events
 //text input
 $("body").keypress(function (e) {
   var charCode = (e.charCode ? e.charCode : e.which);
-  console.log(`Char: ${String.fromCharCode(charCode)}    Code: ${charCode}`);
   userInputManagement(charCode);
 
 });
 //Non-Text Input
 $("body").keydown(function (e) {
   var charCode = (e.charCode ? e.charCode : e.which);
-  console.log(`Char: ${String.fromCharCode(charCode)}    Code: ${charCode}`);
   userInputManagement(charCode, true);
 
 });
