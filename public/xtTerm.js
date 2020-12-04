bed4ba84940335888c3fca8567a927f382c61308
JP <jpjones22@gmail.com>
2020-12-04 08:50:28 -0800

    Added Line and character limitations for the screen to prevent overruns

--- public/xtTerm.js
+++ public/xtTerm.js
@@ -1,10 +1,8 @@
 'use strict'
-// function Neighborhood(rawDataObject) {
-//   // "for in" loop -> for property in object
-//   for (let key in rawDataObject) {
-//     this[key] = rawDataObject[key];
-//   }
-// }
+
+///////////////////////////////
+// 23 Lines and 78 Characters!/
+///////////////////////////////
 
 let maxString = 'Version 3.30 (C)Copyright International Business Machines Corp 1981, 1987';
 let userInput = '';
@@ -12,6 +10,7 @@ let cpuResponseArray = [];
 let monitorArray = [];//['Initial OS 4a50 Systems', 'Initializing Boot Parameters', "[ OK ] Synching mainframe to digital domains", "Connected at (1200) bps", "Running TELNET query", "Response Received - 10b"];
 let monitorLineTracker = 0;
 let maxLinesOnScreen = 23;
+let maxScreenChars = 78;
 let locationDir = '';
 
 //Set's the <p> tags for the number of lined on the screen
@@ -20,27 +19,30 @@ function initMonitor() {
   for (let i = 0; i < maxLinesOnScreen; i++) {
     $('.monitor-readout').append(`<p id="line-${i}"><br />`);
   }
+  commandPrompt();
 }
+
 function returnKeyPressed() {
   commandEvaluation(userInput.toUpperCase());
   console.log('monitor line tracker:', monitorLineTracker);
   console.log('monitor array length', monitorArray.length);
   monitorLineTracker++;
-  if (monitorLineTracker >= maxLinesOnScreen) {
-    console.log('Max Lines Reached on Screen.', maxLinesOnScreen, "Lines");
-    adjustScreenLines();
-  }
+  screenContentManager();
+
 }
 
-function adjustScreenLines() {
-  while (monitorArray >= maxLinesOnScreen);
-  {
-    //console.log('adjusting lines on the screen', monitorArray.length)
-    monitorLineTracker = 22;
-    monitorArray.shift();
-    monitorArray.shift();
-    refreshScreen();
+function screenContentManager() {
+  if (monitorLineTracker >= maxLinesOnScreen) {
+    console.log('Max Lines Reached on Screen.', maxLinesOnScreen, "Lines");
+    while (monitorArray >= maxLinesOnScreen);
+    {
+      monitorLineTracker = 22;
+      monitorArray.shift();
+      monitorArray.shift();
+      refreshScreen();
+    }
   }
+
 }
 /// Main Function to determine what to do with the input
 function userInputManagement(charCode, nonText = false) {
@@ -58,31 +60,42 @@ function userInputManagement(charCode, nonText = false) {
   }
   else {
     // Concatenates user input keystrokes into the userInput array.
-    userInput += String.fromCharCode(charCode);
+    if (userInput.length <= maxScreenChars - 6) {
+      userInput += String.fromCharCode(charCode);
+    }
   }
-  lineText();
+  commandPrompt();
 }
-function lineText() {
+function addText(string = 'No Text Given') {
+  $(`#line-${monitorLineTracker}`).html(string);
+  monitorArray.push(string);
+  monitorLineTracker++;
+  //TODO standardize the line overrun correction if > 23 lines on screen
+}
+function commandPrompt() {
   $(`#line-${monitorLineTracker}`).html(`[${locationDir}]=> ${userInput}`);
 }
-function commandEvaluation(cmdEval) {
+function commandEvaluation(cmdEvalRaw) {
+  let cmdEval = cmdEvalRaw.split(' ');
+
   let screenOutput = '';
   console.log("Processing Request: cmdEval Length:", cmdEval.length);
   //Set the next line, only called after 'RETURN' key pressed
   monitorLineTracker++;
 
-  if (cmdEval.charCodeAt(0) === 13) { cmdEval = cmdEval.slice(1); } // Weeds out the CR that gets put into the string
+  if (cmdEval[0].charCodeAt(0) === 13) { cmdEval[0] = cmdEval[0].slice(1); } // Weeds out the CR that gets put into the string
 
-  if (cmdEval === 'CLS') { console.log('Clearing the Screen'); clearTheScreen(); }
-  else if (!cmdEval) { screenOutput = 'No command given'; console.log('No Entry', monitorLineTracker); }
-  else if (cmdEval === 'JP.EXE') { screenOutput = '-= JP =- is the creator of this little app'; }
-  else if (cmdEval === 'CHKDSK') { screenOutput = 'I\'m not going to check storage.  To much work'; }
-  else if (cmdEval === 'PARKER') { screenOutput = 'Ahh Yes!  The gentleman with the Axolotls!'; }
-  else if (cmdEval === 'SETLOCDIR') { screenOutput = 'Location Directory Set to [rabbit]'; locationDir = 'rabbit'; }
+  if (cmdEval[0] === 'CLS') { console.log('Clearing the Screen'); clearTheScreen(); }
+  else if (!cmdEval[0]) { screenOutput = 'No command given'; console.log('No Entry', monitorLineTracker); }
+  else if (cmdEval[0] === 'JP.EXE') { screenOutput = '-= JP =- is the creator of this little app'; }
+  else if (cmdEval[0] === 'CHKDSK') { screenOutput = 'I\'m not going to check storage.  To much work'; }
+  else if (cmdEval[0] === 'PARKER') { screenOutput = 'Ahh Yes!  The gentleman with the Axolotls!'; }
+  else if (cmdEval[0] === 'SETLOCDIR') { screenOutput = 'Location Directory Set to [rabbit]'; locationDir = 'rabbit'; }
+  else if (cmdEval[0] === 'POKEDEX') { screenOutput = 'Activating PokeDex...'; locationDir = 'pokedex'; pokedex }
   else { screenOutput = 'Bad command or filename'; console.log('Bad command or filename'); }
   monitorArray.push(`[${locationDir}]=> ${userInput}`, screenOutput);
-
   $(`#line-${monitorLineTracker}`).text(screenOutput);
+  commandExec(cmdEval);
   userInput = '';
 }
 
@@ -116,7 +129,21 @@ $("body").keydown(function (e) {
 
 });
 
+function commandExec(userInput) {
+  console.log(userInput);
+  if (userInput[0] === "POKEDEX") {
+    pokedex(userInput[1]);
+  }
+}
 
+function pokedex(userInput) {
+  console.log('Pokemon Command Fired:', userInput);
+  addText(`'You requested and entry on ${userInput}?  Well lets see here.`);
+  addText(`This is interesting, I having trouble finding information on ${userInput}`);
+  addText(`Oh, yes.  There is no API to go fetch.  That makes sense`);
+
+
+}
 
 //Execute//
 
